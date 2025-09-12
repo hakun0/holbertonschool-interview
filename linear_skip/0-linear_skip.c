9baf638dd1d9024fb6af5bf5aaf0ed5e78c91b5f
hakun0 <brithon.saniko@gmail.com>
2025-09-12 16:07:51 +0200

    nvbetty

--- linear_skip/0-linear_skip.c
+++ linear_skip/0-linear_skip.c
@@ -1,6 +1,38 @@
 #include "search.h"
 
 /**
+ * print_check - prints the index and value being checked
+ * @node: pointer to the node
+ */
+static void print_check(skiplist_t *node)
+{
+	if (node != NULL)
+		printf("Value checked at index [%lu] = [%d]\n",
+			node->index, node->n);
+}
+
+/**
+ * search_range - searches linearly in a range of skip list
+ * @list: pointer to the start node
+ * @end: pointer to the end node
+ * @value: value to search for
+ *
+ * Return: pointer to the node if found, NULL otherwise
+ */
+static skiplist_t *search_range(skiplist_t *list,
+		skiplist_t *end, int value)
+{
+	while (list != NULL && list->n <= end->n)
+	{
+		print_check(list);
+		if (list->n == value)
+			return (list);
+		list = list->next;
+	}
+	return (NULL);
+}
+
+/**
  * linear_skip - searches for a value in a sorted skip list
  * @list: pointer to the head of the skip list to search in
  * @value: value to search for
@@ -15,20 +47,14 @@ skiplist_t *linear_skip(skiplist_t *list, int value)
 		return (NULL);
 
 	express = list->express;
-	if (express != NULL)
-		printf("Value checked at index [%lu] = [%d]\n",
-			express->index, express->n);
+	print_check(express);
 
 	while (express != NULL && express->n < value)
 	{
 		list = express;
 		express = express->express;
-
-		if (express == NULL)
-			break;
-
-		printf("Value checked at index [%lu] = [%d]\n",
-			express->index, express->n);
+		if (express != NULL)
+			print_check(express);
 	}
 
 	if (express == NULL)
@@ -41,16 +67,5 @@ skiplist_t *linear_skip(skiplist_t *list, int value)
 	printf("Value found between indexes [%lu] and [%lu]\n",
 		list->index, express->index);
 
-	while (list != NULL && list->n <= express->n)
-	{
-		printf("Value checked at index [%lu] = [%d]\n",
-			list->index, list->n);
-
-		if (list->n == value)
-			return (list);
-
-		list = list->next;
-	}
-
-	return (NULL);
+	return (search_range(list, express, value));
 }

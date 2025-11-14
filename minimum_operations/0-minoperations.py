575c93d40c5b08cc08bfb86e22d2695436e17f6d
hakun0 <brithon.saniko@gmail.com>
2025-11-14 15:11:32 +0100

    minimum

---
+++ minimum_operations/0-minoperations.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Module which contains function"""
+
+
+def minOperations(n):
+    """ Calcules the fewest number of operations
+
+        Args:
+            n: repetitions of H
+
+        Returns:
+            number of operations
+    """
+    if n <= 1:
+        return 0
+    operations = 0
+    divisor = 2
+    while n > 1:
+        while n % divisor == 0:
+            operations += divisor
+            n //= divisor
+        divisor += 1
+
+    return operations
